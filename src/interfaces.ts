08e57f44673a0f80fdfcb9a964244b0c0fcee54b
Eric Crooks <eric.crooks@gmail.com>
2021-03-01 22:27:51 -0500

    add interface descriptions

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,6 +7,12 @@ export type TLogMethod = (messag: string) => void;
 
 /**
  * This CLI's configs.
+ *
+ * name - The CLI's name.
+ * description - The CLI's description.
+ * command - The CLI's main command. This is the command used to run th CLI.
+ * subcommands - An array of Subcommand classes.
+ * version - This CLI's version.
  */
 export interface ICommanderConfigs {
   name: string;
@@ -18,6 +24,11 @@ export interface ICommanderConfigs {
 
 /**
  * This CLI's logger.
+ *
+ * debug - Log a debug message. Outputs "DEBUG some message".
+ * error - Log an error message. Outputs "ERROR some message".
+ * info - Log an info message. Outputs "INFO some message".
+ * warn - Log an warning message. Outputs "WARN some message".
  */
 export interface ILogger {
   debug: TLogMethod;
