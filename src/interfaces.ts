5b5d92bd9984906e8d9ae19294cdd804c98d2a7b
Eric Crooks <eric.crooks@gmail.com>
2021-03-01 22:31:21 -0500

    cleaning up docblocks

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -9,9 +9,13 @@ export type TLogMethod = (messag: string) => void;
  * This CLI's configs.
  *
  * name - The CLI's name.
+ *
  * description - The CLI's description.
+ *
  * command - The CLI's main command. This is the command used to run th CLI.
+ *
  * subcommands - An array of Subcommand classes.
+ *
  * version - This CLI's version.
  */
 export interface ICommanderConfigs {
@@ -26,8 +30,11 @@ export interface ICommanderConfigs {
  * This CLI's logger.
  *
  * debug - Log a debug message. Outputs "DEBUG some message".
+ *
  * error - Log an error message. Outputs "ERROR some message".
+ *
  * info - Log an info message. Outputs "INFO some message".
+ *
  * warn - Log an warning message. Outputs "WARN some message".
  */
 export interface ILogger {
