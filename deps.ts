4c3a5ae4eae145c857e3ac99cc9fd6dfd92e3629
crookse <crookse@users.noreply.github.com>
2021-12-03 00:48:57 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { ConsoleLogger } from "https://raw.githubusercontent.com/drashland/services/v0.2.2/mod.ts";
+export { ConsoleLogger } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/mod.ts";
